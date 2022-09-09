f4e1eee85b9b6c274889a1fd846dcdd81fecf596
Gustavo Dias <gustavodias@cc.ci.ufpb.br>
2022-09-09 14:08:08 -0300

    authentication system - add login page

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,7 +1,8 @@
 class AppRoutes {
+  static const AUTH = '/';
   static const PRODUCT_DETAIL = '/product-detail';
   static const CART = '/cart';
-  static const HOME = '/';
+  static const HOME = '/home';
   static const ORDERS = '/orders';
   static const PRODUCTS = '/products';
   static const PRODUCTS_FORM = '/products-form';
