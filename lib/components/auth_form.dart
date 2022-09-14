3beb2bc6f83a905bcf034f9c459879b104b43bb8
Gustavo Dias <gustavodias@cc.ci.ufpb.br>
2022-09-14 16:02:01 -0300

    using AnimatedBuilder to animate auth page

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -41,7 +41,7 @@ class _AuthFormState extends State<AuthForm>
       end: Size(double.infinity, 400),
     ).animate(CurvedAnimation(parent: _controller!, curve: Curves.linear));
 
-    _heightAnimation?.addListener(() => setState(() {}));
+    // _heightAnimation?.addListener(() => setState(() {}));
   }
 
   @override
@@ -125,10 +125,14 @@ class _AuthFormState extends State<AuthForm>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
-        width: deviceSize.width * 0.75,
+      child: AnimatedBuilder(
+        animation: _heightAnimation!,
+        builder: (context, childForm) => Container(
+          padding: const EdgeInsets.all(16),
+          height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
+          width: deviceSize.width * 0.75,
+          child: childForm,
+        ),
         child: Form(
           key: _formKey,
           child: Column(
